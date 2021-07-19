f4acafed0de8c8099c529a9ccec7728383e01796
Florian <Florian@192.168.1.19>
2021-07-19 14:56:21 +0200

    Création de la classe bll UtilisateursManager

---
+++ ProjetEnchere/src/fr/eni/encheres/bll/UtilisateursManager.java
@@ -0,0 +1,51 @@
+package fr.eni.encheres.bll;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.encheres.bo.Utilisateurs;
+import fr.eni.encheres.dal.DAOFactory;
+import fr.eni.encheres.dal.UtilisateursDAO;
+
+public class UtilisateursManager {
+	
+	
+	private UtilisateursDAO utilisateursDAO;
+	
+	
+	public UtilisateursManager()
+	{
+		this.utilisateursDAO = DAOFactory.getUtilisateursDAO();
+	}
+	
+	
+	public Utilisateurs ajouter(String pseudo, String nom, String prenom,
+								String email, String telephone,String rue, int codePostal, 
+								String ville, String motDePasse,  int credit, int administrateur)
+	{
+		
+		Utilisateurs utilisateur = new Utilisateurs(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse, credit, administrateur);
+		
+		utilisateursDAO.insert(utilisateur);
+		
+		return utilisateur;
+	}
+	
+	public List<Utilisateurs> selectionnerListeUtilisateurs()
+	{
+		List<Utilisateurs> liste = new ArrayList<>();
+		
+		liste = utilisateursDAO.selectAll();
+		
+		return liste;
+		
+	}
+	
+	public Utilisateurs selectionnerUtilisateur(String pseudo)
+	{
+		Utilisateurs utilisateur = utilisateursDAO.selectByPseudo(pseudo);
+		
+		return utilisateur;
+	}
+
+}
