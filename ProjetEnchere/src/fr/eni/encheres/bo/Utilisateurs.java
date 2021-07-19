0b4d02f9e24d18369cd1392152a9d6cea6b5afee
Florian <Florian@192.168.1.19>
2021-07-19 13:33:48 +0200

    Création de la classe Utilisateurs dans la couche bo

--- ProjetEnchere/src/fr/eni/encheres/bo/Utilisateurs.java
+++ ProjetEnchere/src/fr/eni/encheres/bo/Utilisateurs.java
@@ -1,5 +1,130 @@
 package fr.eni.encheres.bo;
 
 public class Utilisateurs {
+	
+
+	private String pseudo;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String motDePasse;
+	private String telephone;
+	private String rue;
+	private int codePostal;
+	private String ville;
+	private int credit;
+	private boolean administrateur;
+	
+	
+	public Utilisateurs(String pseudo, String nom, String prenom, String email, String motDePasse, String telephone, String rue, int codePostal, String ville, int credit, boolean administrateur)
+	{
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.motDePasse = motDePasse;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.credit = credit;
+		this.administrateur = administrateur;
+	}
+	
+	
+
+	public String getPseudo() {
+		return pseudo;
+	}
+	
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getPrenom() {
+		return prenom;
+	}
+	
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getTelephone() {
+		return telephone;
+	}
+	
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	public String getRue() {
+		return rue;
+	}
+	
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	
+	public int getCodePostal() {
+		return codePostal;
+	}
+	
+	public void setCodePostal(int codePostal) {
+		this.codePostal = codePostal;
+	}
+	
+	public String getVille() {
+		return ville;
+	}
+	
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	
+	public int getCredit() {
+		return credit;
+	}
+	
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	
+	public boolean isAdministrateur() {
+		return administrateur;
+	}
+	
+	public void setAdministrateur(boolean administrateur) {
+		this.administrateur = administrateur;
+	}
+
+
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+	
+
 
 }
