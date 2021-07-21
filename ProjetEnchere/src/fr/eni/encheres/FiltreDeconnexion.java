1c4a924aae12e5d43bc4745a8ae153dd07c3ae0e
Florian <Florian@192.168.1.19>
2021-07-21 09:51:15 +0200

    Création d'une servlet déconnexion et son filtre associé

---
+++ ProjetEnchere/src/fr/eni/encheres/FiltreDeconnexion.java
@@ -0,0 +1,71 @@
+package fr.eni.encheres;
+
+import java.io.IOException;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class FiltreDeconnexion
+ */
+@WebFilter(urlPatterns ="/deconnexion",
+dispatcherTypes = {
+		   				DispatcherType.REQUEST,
+		   				DispatcherType.INCLUDE,
+		   				DispatcherType.FORWARD,
+		   				DispatcherType.ERROR
+					  }
+
+
+)
+public class FiltreDeconnexion implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public FiltreDeconnexion() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpSession session = httpRequest.getSession();
+		if(session.getAttribute("connexion") != "true")
+		{
+			RequestDispatcher rd = request.getRequestDispatcher("/connexion");
+			rd.forward(httpRequest, response);
+		}
+		else
+		{
+		chain.doFilter(request, response);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ ProjetEnchere/src/fr/eni/encheres/servlets/ServletDeconnexion.java
@@ -0,0 +1,55 @@
+package fr.eni.encheres.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ServletDeconnexion
+ */
+@WebServlet("/deconnexion")
+public class ServletDeconnexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletDeconnexion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		
+		RequestDispatcher rd = null;
+		
+		if(session.getAttribute("connexion") == "true")
+		{
+			session.setAttribute("connexion", "false");
+			session.setAttribute("utilisateur", null);
+			rd = request.getRequestDispatcher("/accueil");
+			rd.forward(request, response);
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
