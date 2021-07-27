b077357115fc012b1b9aad0ce34bb645e3f5daef
moi <moi@ELIE-LAPTOP>
2021-07-27 12:40:30 +0200

    recherche + codes d'erreur

--- ProjetEnchere/src/fr/eni/encheres/dal/ArticlesVendusDAOJdbcImpl.java
+++ ProjetEnchere/src/fr/eni/encheres/dal/ArticlesVendusDAOJdbcImpl.java
@@ -27,6 +27,17 @@ public class ArticlesVendusDAOJdbcImpl implements ArticlesVendusDAO{
 			+ " INNER JOIN Categories c ON a.no_categorie = c.no_categorie"
 			+ " WHERE c.libelle=?;";
 	
+	private static final String rechercherArticlesParCategorie = "SELECT no_article, nom_article, description, date_fin_encheres,"
+			+ "prix_vente, pseudo FROM Articles_Vendus a"
+			+ " INNER JOIN Utilisateurs u ON a.no_utilisateur = u.no_utilisateur"
+			+ " INNER JOIN Categories c ON a.no_categorie = c.no_categorie"
+			+ " WHERE c.libelle=? AND a.nom_article like %?%;";
+	
+	private static final String rechercherArticlesSansCategorie = "SELECT no_article, nom_article, description, date_fin_encheres,"
+			+ "prix_vente, pseudo FROM Articles_Vendus a"
+			+ " INNER JOIN Utilisateurs u ON a.no_utilisateur = u.no_utilisateur"
+			+ " WHERE a.nom_article like %?%;";
+	
 	private static final String selectById = "SELECT no_article, nom_article, description, date_debut_encheres,"
 			+ " date_fin_encheres, prix_initial, no_utilisateur, no_categorie "
 			+ " FROM Articles_Vendus WHERE no_article=?;";
@@ -116,6 +127,47 @@ public class ArticlesVendusDAOJdbcImpl implements ArticlesVendusDAO{
 			e.printStackTrace();
 		}
 		return unArticle;
+	}
+	
+	@Override
+	public List<ArticlesVendus> rechercherArticlesParCategorie(String recherche, String nomCategorie)
+	{
+		List<ArticlesVendus> donneesArticles = new ArrayList<>();
+		try(Connection cnx = ConnectionProvider.getConnection())
+		{
+			PreparedStatement pstmt = cnx.prepareStatement(rechercherArticlesParCategorie);
+			pstmt.setString(1, nomCategorie);
+			pstmt.setString(2, recherche);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next())
+			{
+				donneesArticles.add(new ArticlesVendus(rs.getInt(1), rs.getString(2), rs.getString(3),
+						rs.getDate(4),rs.getInt(5), rs.getString(6)));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return donneesArticles;
+	}
+	
+	@Override
+	public List<ArticlesVendus> rechercherArticlesSansCategorie(String recherche)
+	{
+		List<ArticlesVendus> donneesArticles = new ArrayList<>();
+		try(Connection cnx = ConnectionProvider.getConnection())
+		{
+			PreparedStatement pstmt = cnx.prepareStatement(rechercherArticlesSansCategorie);
+			pstmt.setString(1, recherche);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next())
+			{
+				donneesArticles.add(new ArticlesVendus(rs.getInt(1), rs.getString(2), rs.getString(3),
+						rs.getDate(4),rs.getInt(5), rs.getString(6)));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return donneesArticles;
 	}
 
 	@Override
